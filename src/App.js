08c0d46720f84a36ff37e5580a000492f2636b63
Kyra Hrabal <kyra.hrabal@sri.com>
2019-06-24 18:00:58 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmployeeList></EmployeeList>
     </div>
   );
+}
+
+class EmployeeList extends React.Component {
+  render() {
+    return (
+      <div className='employee-list'>
+        <h1>Employee list for {this.props.company}</h1>
+        <EmployeeCard></EmployeeCard>
+      </div>
+    );
+  }
+}
+
+class EmployeeCard extends React.Component {
+  render() {
+    return (
+      <div className='employee-card card'>
+        <div className='card-body'>
+          <h5 class='card-title'>Employee 1</h5> 
+          <p className='card-text'>Hard worker, highly motivated, fast learner.</p>
+          <a href="#" class="btn btn-primary">View Skills</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
