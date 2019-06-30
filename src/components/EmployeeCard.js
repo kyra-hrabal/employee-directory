50d72c749bf45c25c8ef5bc0bfb8643b384fbd81
Kyra Hrabal <kyra.hrabal@sri.com>
2019-06-30 12:47:36 -0400

    Removed static mock data and fixed image loading isue

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,62 +1,12 @@
 import React from 'react';
 import '../css/App.css';
 
-// Mock employee data set
-// var employees = [
-//   {
-//     'name': 'Kathy Hurst',
-//     'description': 'Intelligent, harder worker.',
-//     'image': kathyhurst,
-//     'location': 'Saint Petersburg, FL',
-//     'title': 'Project Manager'
-//   },
-//   {
-//     'name': 'David Brown',
-//     'description': 'Imaginative designer and artist.',
-//     'image': davidbrown,
-//     'location': 'New York, NY',
-//     'title': 'Senior Software Engineer'
-//   },
-//   {
-//     'name': 'Holly Howard',
-//     'description': 'Funny, clever, witty.',
-//     'image': hollyhoward,
-//     'location': 'Saint Petersburg, FL',
-//     'title': 'Executive Assistant'
-//   },
-//   {
-//     'name': 'Miranda Ginsburg',
-//     'description': 'Motivated',
-//     'image': '',
-//     'location': 'New York, NY',
-//     'title': 'Project Manager'
-//   },
-//   {
-//     'name': 'William Duntree',
-//     'description': 'Super friendly.',
-//     'image': '',
-//     'location': 'Portland, OR',
-//     'title': 'Software Engineer'
-//   },
-//   {
-//     'name': 'Kelly Smith',
-//     'description': 'Smart',
-//     'image': '',
-//     'location': 'New York, NY',
-//     'title': 'Software Engineer'
-//   }
-// ]
-
+// Import mock employee data
 var data = require('../employees.json');
 console.log(data);
 var employees=data.employees;
 console.log(employees);
-employees.map(employee => 
-  console.log(employee.image)
-)
-// Require context image folder
-const images = require.context('../img', true);
-console.log(images);
+
 
 class EmployeeCard extends React.Component {
   constructor(props) {
@@ -64,18 +14,13 @@ class EmployeeCard extends React.Component {
     this.state = {
       name: null,
     };
-  //   var employeeGroup = []
-  //   if (this.props.group == '1')      
-  //     employeeGroup = employees.slice(3);
-  //   else
-  //     employeeGroup = employees.slice(4, -1);
 
   }
   render() {
     return (
       employees.map(employee =>
         <div className='employee-card card'>
-          <img src={employee.image} className="card-img-top" alt="..."></img>
+          <img src={process.env.PUBLIC_URL + employee.image} className="card-img-top" alt="..."></img>
           <div className='card-body'>
             <h5 className='card-title'>{employee.name}</h5> 
             <p className='card-text'>{employee.description}</p>
--- src/components/EmployeeTableEntry.js
+++ src/components/EmployeeTableEntry.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import '../css/Admin.css';
-import kathyhurst from '../img/kathyhurst.jpg';
-import davidbrown from '../img/davidbrown.jpg';
-import hollyhoward from '../img/hollyhoward.jpg';
 
-var employees = [
-    {
-        'name': 'Kathy Hurst',
-        'description': 'Intelligent, harder worker.',
-        'image': kathyhurst,
-        'location': 'Saint Petersburg, FL',
-        'title': 'Project Manager'
-    },
-    {
-        'name': 'David Brown',
-        'description': 'Imaginative designer and artist.',
-        'image': davidbrown,
-        'location': 'New York, NY',
-        'title': 'Senior Software Engineer'
-    },
-    {
-        'name': 'Holly Howard',
-        'description': 'Funny, clever, witty.',
-        'image': hollyhoward,
-        'location': 'Saint Petersburg, FL',
-        'title': 'Executive Assistant'
-    }
-]
+// Import mock employee data
+var data = require('../employees.json');
+console.log(data);
+var employees=data.employees;
+console.log(employees);
 
 class EmployeeTableEntry extends React.Component{
     render() {
@@ -34,7 +13,7 @@ class EmployeeTableEntry extends React.Component{
             employees.map(employee =>
             <tr className='employee-row'>
             <th scope="row">1</th>
-            <img className='employee-image' src={employee.image} alt='' />
+            <img className='employee-image' src={process.env.PUBLIC_URL + employee.image} alt='' />
             <td>{employee.name}</td>
             <td>{employee.location}</td>
             <td>{employee.title}</td>
